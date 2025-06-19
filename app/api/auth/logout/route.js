8dc772c55d972a5a18d12664cd845b774bbd1ddf
Mark Bathie <mbathie@gmail.com>
2025-06-19 15:54:45 +1000

    wip

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    const response = NextResponse.redirect(`${process.env.HOST}/`); // Use an absolute URL
+    const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_API_BASE_URL}/`); // Use an absolute URL
     response.cookies.set("token", "", { httpOnly: true, secure: true, expires: new Date(0) });
 
     return response;
