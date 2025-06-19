e1221379a8a802962f7fd8fcc6ee24198d9cfc90
Mark Bathie <mbathie@gmail.com>
2025-06-19 15:23:02 +1000

    wip

--- app/api/employees/[id]/route.js
+++ app/api/employees/[id]/route.js
@@ -1,26 +1,26 @@
-import { NextResponse } from "next/server"
-import prisma from "@/lib/db"
+// import { NextResponse } from "next/server"
+// import prisma from "@/lib/db"
 
-export async function PUT(req, { params }) {
-  try {
+// export async function PUT(req, { params }) {
+//   try {
 
-    const { id } = await params
-    const body = await req.json()
-    const { name, email, locationId, role } = body
+//     const { id } = await params
+//     const body = await req.json()
+//     const { name, email, locationId, role } = body
 
-    if (!name || !email || !locationId)
-      return NextResponse.json({ error: "Missing required fields" }, { status: 400 })
+//     if (!name || !email || !locationId)
+//       return NextResponse.json({ error: "Missing required fields" }, { status: 400 })
 
-    const newEmployee = await prisma.employee.update({
-      where: { id: parseInt(id) },
-      data: { name, email, locationId, role },
-      omit: { hash: true },
-      include: { location: true }
-    })
+//     const newEmployee = await prisma.employee.update({
+//       where: { id: parseInt(id) },
+//       data: { name, email, locationId, role },
+//       omit: { hash: true },
+//       include: { location: true }
+//     })
 
-    return NextResponse.json({...newEmployee}, { status: 201 })
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
-  }
-}
+//     return NextResponse.json({...newEmployee}, { status: 201 })
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+//   }
+// }
--- app/api/locations/[id]/products/route.js
+++ app/api/locations/[id]/products/route.js
@@ -1,42 +1,42 @@
-import { NextResponse } from 'next/server'
-import prisma from "@/lib/db"
+// import { NextResponse } from 'next/server'
+// import prisma from "@/lib/db"
 
-export async function PUT(req, { params }) {
-  try {
+// export async function PUT(req, { params }) {
+//   try {
 
-    const { id } = await params;
-    const locationId = parseInt(id)
-    const { products } = await req.json();
+//     const { id } = await params;
+//     const locationId = parseInt(id)
+//     const { products } = await req.json();
 
-    if (!locationId || !Array.isArray(products)) {
-      return NextResponse.json({ error: 'Invalid payload' }, { status: 400 })
-    }
+//     if (!locationId || !Array.isArray(products)) {
+//       return NextResponse.json({ error: 'Invalid payload' }, { status: 400 })
+//     }
 
-    const updates = await Promise.all(
-      products.map(async ({ productId, enabled }) => {
-        await prisma.productLocation.upsert({
-          where: {
-            locationId_productId: {
-              locationId,
-              productId,
-            },
-          },
-          update: {
-            enabled,
-          },
-          create: {
-            locationId,
-            productId,
-            enabled,
-          },
-        });
+//     const updates = await Promise.all(
+//       products.map(async ({ productId, enabled }) => {
+//         await prisma.productLocation.upsert({
+//           where: {
+//             locationId_productId: {
+//               locationId,
+//               productId,
+//             },
+//           },
+//           update: {
+//             enabled,
+//           },
+//           create: {
+//             locationId,
+//             productId,
+//             enabled,
+//           },
+//         });
         
-      })
-    )
+//       })
+//     )
 
-    return NextResponse.json({ success: true, updates })
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json({ error: 'Server error' }, { status: 500 })
-  }
-}
+//     return NextResponse.json({ success: true, updates })
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json({ error: 'Server error' }, { status: 500 })
+//   }
+// }
